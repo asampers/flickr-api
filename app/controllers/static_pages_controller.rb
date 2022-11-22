cdff387a1a7da13989c79f11a6f8ee772113799e
nanarama <annasampers@gmail.com>
2022-11-22 17:06:02 -0600

    Fix image and rescue clause

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,17 +2,12 @@ class StaticPagesController < ApplicationController
   require 'flickr'
 
   def index
-    begin 
       flickr = Flickr.new(ENV['flickr_key'], ENV['flickr_secret'])
 
       unless params[:user_id].blank?
         @photos = flickr.photos.search(user_id: params[:user_id])
         @user = flickr.people.getInfo(user_id: "#{@photos[0].owner}")
-      end  
-    rescue StandardError => e
-      flash[:alert] = "Oops, that doesn't work. Please try again..."
-      #redirect_to root_path
-    end    
+      end     
   end
 
 end
